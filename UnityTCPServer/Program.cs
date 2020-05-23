f23ef955412bbc2fea59a13edfe2a0a956bf1f2b
Ben5460 <chengye@deakin.edu.au>
2020-05-24 03:54:58 +1000

    Add files via upload

---
+++ UnityTCPServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using System;
+
+namespace TCPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread serverThread = new Thread(() => new Server("192.168.0.47" , 8080));
+            serverThread.Start();
+            
+            // server is going to be launch from here
+            Console.WriteLine("Server Started...192.168.0.47");
+        }
+    }
+}
